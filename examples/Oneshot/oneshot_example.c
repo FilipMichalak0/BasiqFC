7a67a0ec7eb556de67b06af050e21d9eee46d630
u8101081741-source <u8101081741@gmail.com>
2026-01-03 17:57:46 +0100

    My file just left but its not final its for setting up esc's

---
+++ examples/Oneshot/oneshot_example.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/clocks.h"
+#include "hardware/pwm.h"
+#include "oneshot.h"
+#include "crsf.h"
+
+int main()
+{
+    set_sys_clock_hz(220000000,true);
+    stdio_init_all();
+    crsf_data CRSF = {
+        .UartCRSFPort = uart0,
+        .UartTxPin = 12,
+        .UartRxPin = 13
+    };
+    CRSF_Init(&CRSF);
+    oneshot Oneshot = {
+        .motorLF = 14,
+        //.motorLB = 15,
+        //.motorRF = 16,
+        //.motorRB = 17
+    };
+    ONESHOT_initMotors(&Oneshot);
+
+    while (true) 
+    {
+        CRSF_StateMachine(&CRSF);
+        CRSF_ChannelMaping(&CRSF);
+        /*
+        printf("ch1 = %d ", Crsf.PWMData[0]);
+        printf("ch2 = %d ", Crsf.PWMData[1]);
+        printf("ch3 = %d ", Crsf.PWMData[2]);
+        printf("ch4 = %d  \n", Crsf.PWMData[3]);
+        */
+        Oneshot.fillLF = (CRSF.PWMData[0] * 125) / 1000;
+        if(Oneshot.fillLF > 240) Oneshot.fillLF = 240;
+        //Oneshot.fillLB = (CRSF.PWMData[1] * 125) / 1000;
+        //if(Oneshot.fillLB > 240) Oneshot.fillLB = 240;
+        //Oneshot.fillRF = (CRSF.PWMData[2] * 125) / 1000;
+        //if(Oneshot.fillRF > 240) Oneshot.fillRF = 240;
+        //Oneshot.fillRB = (CRSF.PWMData[3] * 125) / 1000;
+        //if(Oneshot.fillRB > 240) Oneshot.fillRB = 240;
+        printf("LF = %d ", Oneshot.fillLF);
+       // printf("LB = %d ", Oneshot.fillLB);
+       //printf("RF = %d ", Oneshot.fillRF);
+        //printf("RB = %d  \n", Oneshot.fillRB);
+        ONESHOT_writeMotors(&Oneshot);
+        sleep_ms(5);
+    }
+}
